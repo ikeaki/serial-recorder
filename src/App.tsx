22169583a1f788a769ee3da2e986c349a33f119e
ikeaki <ikeaki222@gmail.com>
2026-09-23 12:18:27 +0900

    Phase3B EXL FIle Name

--- src/App.tsx
+++ src/App.tsx
@@ -138,9 +138,26 @@ export default function App() {
       "履歴"
     );
 
+    const now = new Date();
+
+    const fileName =
+      `serial-history-${
+        now.getFullYear()
+      }${
+        String(now.getMonth() + 1).padStart(2, "0")
+      }${
+        String(now.getDate()).padStart(2, "0")
+      }-${
+        String(now.getHours()).padStart(2, "0")
+      }${
+        String(now.getMinutes()).padStart(2, "0")
+      }${
+        String(now.getSeconds()).padStart(2, "0")
+      }.xlsx`;
+
     XLSX.writeFile(
       workbook,
-      "serial-history.xlsx"
+      fileName
     );
   };
   
